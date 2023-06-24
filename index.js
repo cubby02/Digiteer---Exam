a69fb4c20c184939be89d642f9f35223baca4966
NJ <noeljustine.mendoza@gmail.com>
2023-06-24 20:46:08 +0800

    working contact us

--- index.js
+++ index.js
@@ -24,3 +24,57 @@ window.addEventListener('scroll', () => {
         document.getElementById('navBrandLogo').src = 'assets/images/logo-only-colored.png';
     }
 });
+
+function show_alert() {
+    const txtFirstName = document.getElementById('txtFirstName');
+    const txtLasttName = document.getElementById('txtLasttName');
+    const txtEmail = document.getElementById('txtEmail');
+    const error_elements = ['fnError','lnError','emailError'];
+    const notEmail = document.getElementById('notEmail');
+
+    if (isNotEmpty(txtFirstName.value) && isNotEmpty(txtLasttName.value) &&isNotEmpty(txtEmail.value) ){
+        if(isEmail(txtEmail.value)) {
+            notEmail.style.display = 'none';
+            txtEmail.classList.remove('field_error');
+
+            alert('First Name: ' + txtFirstName.value + " " + txtLasttName.value + "\nEmail: " + txtEmail.value); 
+
+            txtFirstName.value = '';
+            txtLasttName.value = '';
+            txtEmail.value = '';
+              
+        } else {
+            txtFirstName.classList.remove('field_error');
+            txtLasttName.classList.remove('field_error');
+
+            error_elements.forEach((elementId) => {
+                const element = document.getElementById(elementId);
+                element.style.display = 'none';
+            });
+
+            notEmail.style.display = 'block';
+            txtEmail.classList.add('field_error');
+        }
+    } else {
+        error_elements.forEach((elementId) => {
+            const element = document.getElementById(elementId);
+            element.style.display = 'block';
+        });
+
+        txtFirstName.classList.add('field_error');
+        txtLasttName.classList.add('field_error');
+        txtEmail.classList.add('field_error');
+    }
+    
+
+}
+
+function isNotEmpty(value) {
+    if (value == null || typeof value == 'undefined' ) return false;
+    return (value.length > 0);
+}
+
+function isEmail(email) {
+    let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+    return regex.test(String(email).toLowerCase());
+}
